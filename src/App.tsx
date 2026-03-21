4466047af9a3c0d70e9ac5f2d714de5d90de9278
이희원 <richready2011@gmail.com>
2026-03-22 05:54:00 +0900

    fix: base 스타일 조정

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { AuthProvider } from '@/context/AuthContext'
 
+import './App.scss'
+
 import ProtectedRoute from '@/layout/ProtectedRoute'
 import DefaultLayout from '@/layout/Default'
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.tsx'
-import './main.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
